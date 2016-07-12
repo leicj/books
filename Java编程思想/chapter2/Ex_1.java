818070363062670dabc28ed7ae2539cd35dbb33c
leicj <leichaojian@outlook.com>
2016-07-13 07:52:23 +0800

    添加Java编程思想

---
+++ Java编程思想/chapter2/Ex_1.java
@@ -0,0 +1,12 @@
+/**
+ * Created by lgt on 16/7/12.
+ */
+public class Ex_1 {
+  int i;
+  char c;
+  public static void main(String[] args) {
+    Ex_1 ex = new Ex_1();
+    // int:0; char:
+    System.out.println("int:" + ex.i + "; char:" + ex.c);
+  }
+}
---
+++ Java编程思想/chapter2/Ex_2.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lgt on 16/7/12.
+ */
+public class Ex_2 {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
